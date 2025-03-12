5ed68b090ed2acf33952a30a578255abb4b5586b
juriber <juriberodr1@educacion.navarra.es>
2025-03-12 12:53:45 +0100

    documentación

--- Documentacion/uribe_DEAW06/js/searchIndex.js
+++ Documentacion/uribe_DEAW06/js/searchIndex.js
@@ -11,6 +11,11 @@ Search.appendIndex(
             "summary": "Funci\u00F3n\u0020que\u0020genera\u0020un\u0020saludo\u0020personalizado.",
             "url": "namespaces/default.html#function_saludo"
         },                {
+            "fqsen": "\\area_circulo\u0028\u0029",
+            "name": "area_circulo",
+            "summary": "Calcula\u0020el\u0020\u00E1rea\u0020de\u0020un\u0020c\u00EDrculo\u0020dado\u0020su\u0020radio.",
+            "url": "namespaces/default.html#function_area_circulo"
+        },                {
             "fqsen": "\\",
             "name": "\\",
             "summary": "",
